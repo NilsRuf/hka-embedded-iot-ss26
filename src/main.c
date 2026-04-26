0accb56a877ddd052db36a8d35a763da7307e9a5
Nils Ruf <nils.ruf@protonmail.com>
2026-04-26 11:23:30 +0200

    broadcast temperature

--- src/main.c
+++ src/main.c
@@ -1,52 +1,101 @@
+// Small BLE sample.
 
-#include <stdio.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 // Define a log module and assign it a log priority.
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
-// Here we can access our prj.conf/Kconfig definitions
-#define SLEEP_TIME_LED0_MS   CONFIG_LED0_BLINK_INTERVAL_MS
+static uint8_t manufacturer_data[] = {0xFF, 0xFF, 0x00, 0x00};
 
-// These come from the devicetree
-#define LED0_NODE DT_ALIAS(led0)
+// Advertizing data
+static const struct bt_data advertizing_data[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR),
+    BT_DATA(BT_DATA_MANUFACTURER_DATA, manufacturer_data, sizeof(manufacturer_data)),
+};
 
-// In the next line we retrieve the GPIO node defined in the device tree.
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+// Only read when a device explicitly sends a SCAN request.
+static const struct bt_data scan_data[] = {
+    BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+};
+
+static const struct device *sensor = DEVICE_DT_GET(DT_NODELABEL(bmp280));
+
+K_SEM_DEFINE(bt_ready, 0, 1);
+
+static void on_ble_enable(int err)
+{
+    if (err != 0) {
+        LOG_ERR("Failed to init stack: %d", err);
+        return;
+    }
+
+    k_sem_give(&bt_ready);
+}
 
 int main(void)
 {
-    int ret;
-    bool led0_on = true;
-
-    // Basically never fails. Each device has a function to check if it is ready.
-    // If not, it indicates an error during initialization.
-    if (!gpio_is_ready_dt(&led)) {
+    int err = bt_enable(on_ble_enable);
+    if (err != 0) {
+        LOG_ERR("That did not work :( %d", err);
         return 0;
     }
 
-    // The following function is used to set a GPIO's direction to output. GPIO_OUTPUT_ACTIVE means
-    // to set the pin to its ACTIVE level.
-    // The ACTIVE level is defined in the devicetree as well and can be either 1 (active high)
-    // or 0 (active low).
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
+    if (!device_is_ready(sensor)) {
+        LOG_ERR("Sensor not ready");
         return 0;
     }
 
-    while (1) {
-        LOG_INF("LED0 is %s", led0_on ? "on" : "off");
-        led0_on = !led0_on;
+    (void)k_sem_take(&bt_ready, K_FOREVER);
+    LOG_INF("Bluetooth stack is initialized - start advertizing");
 
-        k_msleep(SLEEP_TIME_LED0_MS);
+    err = bt_le_adv_start(BT_LE_ADV_NCONN, advertizing_data, ARRAY_SIZE(advertizing_data),
+                          scan_data, ARRAY_SIZE(scan_data));
+    if (err != 0) {
+        LOG_ERR("Failed to start advertising: %d", err);
+        return 0;
+    }
 
-        // Here we toggle the GPIO
-        ret = gpio_pin_toggle_dt(&led);
-        if (ret < 0) {
-            return 0;
+    LOG_INF("Advertising! :)");
+
+    for (;;) {
+        err = sensor_sample_fetch(sensor);
+        if (err != 0) {
+            LOG_ERR("Failed to fetch sample: %d", err);
+            goto sleep;
         }
+
+        struct sensor_value temp;
+        err = sensor_channel_get(sensor, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+        if (err != 0) {
+            LOG_ERR("Failed to get temperature: %d", err);
+            goto sleep;
+        }
+
+        int16_t adv_temp;
+        if (temp.val1 >= 0) {
+            adv_temp = (temp.val1 * 100) + (temp.val2 / 10000);
+        } else {
+            adv_temp = (temp.val1 * 100) - (temp.val2 / 10000);
+        }
+        LOG_INF("Temperature: %d.%d°C -> %d", temp.val1, temp.val2, adv_temp);
+
+        manufacturer_data[2] = adv_temp >> 8;
+        manufacturer_data[3] = adv_temp & 0xFF;
+
+        err = bt_le_adv_update_data(advertizing_data, ARRAY_SIZE(advertizing_data), scan_data,
+                                    ARRAY_SIZE(scan_data));
+        if (err != 0) {
+            LOG_ERR("Failed to update advertizing data: %d", err);
+        }
+
+    sleep:
+        k_msleep(1000);
     }
     return 0;
 }
