39ce86816a5f2930ac1899a43f8f8c05a085c703
Nils Ruf <nils.ruf@protonmail.com>
2026-04-26 11:51:42 +0200

    connect and disconnect

--- src/main.c
+++ src/main.c
@@ -13,6 +13,8 @@ LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 static uint8_t manufacturer_data[] = {0xFF, 0xFF, 0x00, 0x00};
 
+static atomic_t is_connected = ATOMIC_INIT(0);
+
 // Advertizing data
 static const struct bt_data advertizing_data[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR),
@@ -27,6 +29,10 @@ static const struct bt_data scan_data[] = {
 static const struct device *sensor = DEVICE_DT_GET(DT_NODELABEL(bmp280));
 
 K_SEM_DEFINE(bt_ready, 0, 1);
+K_SEM_DEFINE(advertize, 0, 1);
+
+static void restart_advertizing(struct k_work *work);
+K_WORK_DEFINE(adv_work, restart_advertizing);
 
 static void on_ble_enable(int err)
 {
@@ -37,6 +43,29 @@ static void on_ble_enable(int err)
 
     k_sem_give(&bt_ready);
 }
+
+static void connected(__maybe_unused struct bt_conn *conn, uint8_t err)
+{
+    if (err != 0) {
+        LOG_ERR("Connection attempt failed: %d", err);
+        return;
+    }
+
+    (void)atomic_set(&is_connected, 1);
+    LOG_INF("Client connected");
+}
+
+static void disconnected(__maybe_unused struct bt_conn *conn, uint8_t reason)
+{
+    LOG_INF("Client disconnected - reason %d", reason);
+    (void)atomic_set(&is_connected, 0);
+    (void)k_work_submit(&adv_work);
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+    .connected = connected,
+    .disconnected = disconnected,
+};
 
 int main(void)
 {
@@ -54,7 +83,7 @@ int main(void)
     (void)k_sem_take(&bt_ready, K_FOREVER);
     LOG_INF("Bluetooth stack is initialized - start advertizing");
 
-    err = bt_le_adv_start(BT_LE_ADV_NCONN, advertizing_data, ARRAY_SIZE(advertizing_data),
+    err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, advertizing_data, ARRAY_SIZE(advertizing_data),
                           scan_data, ARRAY_SIZE(scan_data));
     if (err != 0) {
         LOG_ERR("Failed to start advertising: %d", err);
@@ -64,6 +93,10 @@ int main(void)
     LOG_INF("Advertising! :)");
 
     for (;;) {
+        if (atomic_get(&is_connected) == 1) {
+            k_sem_take(&advertize, K_FOREVER);
+        }
+
         err = sensor_sample_fetch(sensor);
         if (err != 0) {
             LOG_ERR("Failed to fetch sample: %d", err);
@@ -98,4 +131,16 @@ int main(void)
         k_msleep(1000);
     }
     return 0;
+}
+
+static void restart_advertizing(struct k_work *work)
+{
+    int err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, advertizing_data, ARRAY_SIZE(advertizing_data),
+                              scan_data, ARRAY_SIZE(scan_data));
+    if (err != 0) {
+        LOG_ERR("Failed to update advertizing data: %d", err);
+    }
+
+    LOG_INF("Advertising again...");
+    k_sem_give(&advertize);
 }
